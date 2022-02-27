0ae997e4560150322c4bbad497d01660763fd473
Christopher Jeffrey <chjjeffrey@gmail.com>
2022-02-27 05:48:08 -0500

    begin work on db_impl

--- src/db_impl.c
+++ src/db_impl.c
@@ -1,1 +1,41 @@
+/*!
+ * db_impl.c - database implementation for rdb
+ * Copyright (c) 2022, Christopher Jeffrey (MIT License).
+ * https://github.com/chjj/rdb
+ */
 
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "table/block.h"
+#include "table/iterator.h"
+#include "table/merger.h"
+#include "table/table.h"
+#include "table/table_builder.h"
+#include "table/two_level_iterator.h"
+
+#include "util/array.h"
+#include "util/atomic.h"
+#include "util/buffer.h"
+#include "util/coding.h"
+#include "util/comparator.h"
+#include "util/env.h"
+#include "util/internal.h"
+#include "util/options.h"
+#include "util/port.h"
+#include "util/rbt.h"
+#include "util/slice.h"
+#include "util/status.h"
+#include "util/vector.h"
+
+#include "builder.h"
+#include "db_iter.h"
+#include "dbformat.h"
+#include "filename.h"
+#include "log_format.h"
+#include "log_reader.h"
+#include "log_writer.h"
+#include "table_cache.h"
+#include "version_set.h"
