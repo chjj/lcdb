69bff210ddab2d1381f87fb705e550a51894bba2
Christopher Jeffrey <chjjeffrey@gmail.com>
2022-03-10 13:43:45 -0500

    remove copyright from code not derived from leveldb

--- src/util/extern.h
+++ src/util/extern.h
@@ -2,12 +2,6 @@
  * extern.h - extern definitions for lcdb
  * Copyright (c) 2022, Christopher Jeffrey (MIT License).
  * https://github.com/chjj/lcdb
- *
- * Parts of this software are based on google/leveldb:
- *   Copyright (c) 2011, The LevelDB Authors. All rights reserved.
- *   https://github.com/google/leveldb
- *
- * See LICENSE for more information.
  */
 
 #ifndef LDB_EXTERN_H
--- src/util/vector.h
+++ src/util/vector.h
@@ -2,12 +2,6 @@
  * vector.h - shallow vector for lcdb
  * Copyright (c) 2022, Christopher Jeffrey (MIT License).
  * https://github.com/chjj/lcdb
- *
- * Parts of this software are based on google/leveldb:
- *   Copyright (c) 2011, The LevelDB Authors. All rights reserved.
- *   https://github.com/google/leveldb
- *
- * See LICENSE for more information.
  */
 
 #ifndef LDB_VECTOR_H
