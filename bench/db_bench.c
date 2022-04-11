00dca498bd54d0f103f5f783176056be0f3c5df7
Christopher Jeffrey <chjjeffrey@gmail.com>
2022-04-11 04:45:32 -0400

    bench: stub benchmarks.

---
+++ bench/db_bench.c
@@ -0,0 +1,24 @@
+/*!
+ * db_bench.c - database benchmarks for lcdb
+ * Copyright (c) 2022, Christopher Jeffrey (MIT License).
+ * https://github.com/chjj/lcdb
+ *
+ * Parts of this software are based on google/leveldb:
+ *   Copyright (c) 2011, The LevelDB Authors. All rights reserved.
+ *   https://github.com/google/leveldb
+ *
+ * See LICENSE for more information.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  printf("TODO\n");
+  return 1;
+}
