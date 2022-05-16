4d080ea9f7f4757ba5d3a6a7f8b7775c28ea599c
Christopher Jeffrey <chjjeffrey@gmail.com>
2022-05-16 03:37:15 -0400

    contrib/lwdb: minor.

--- contrib/lwdb/lwdb.c
+++ contrib/lwdb/lwdb.c
@@ -387,12 +387,8 @@ safe_malloc(size_t size) {
 
 static void
 safe_free(void *ptr) {
-  if (ptr == NULL) {
-    abort(); /* LCOV_EXCL_LINE */
-    return;
-  }
-
-  free(ptr);
+  if (ptr != NULL)
+    free(ptr);
 }
 
 static int
