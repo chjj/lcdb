3ffd43c18b99366821dc283e75492df6f9f1277f
Christopher Jeffrey <chjjeffrey@gmail.com>
2022-08-20 20:59:24 -0400

    util/buffer: optimize buffer_escape.

--- src/util/buffer.c
+++ src/util/buffer.c
@@ -185,10 +185,9 @@ ldb_buffer_number(ldb_buffer_t *z, uint64_t x) {
 
 void
 ldb_buffer_escape(ldb_buffer_t *z, const ldb_slice_t *x) {
+  static const char *nibble = "0123456789abcdef";
   uint8_t *zp = ldb_buffer_expand(z, x->size * 4 + 1);
   size_t i;
-
-#define nibble(x) ((x) < 10 ? (x) + '0' : (x) - 10 + 'a')
 
   for (i = 0; i < x->size; i++) {
     int ch = x->data[i];
@@ -198,12 +197,10 @@ ldb_buffer_escape(ldb_buffer_t *z, const ldb_slice_t *x) {
     } else {
       *zp++ = '\\';
       *zp++ = 'x';
-      *zp++ = nibble(ch >> 4);
-      *zp++ = nibble(ch & 15);
+      *zp++ = nibble[ch >> 4];
+      *zp++ = nibble[ch & 15];
     }
   }
-
-#undef nibble
 
   z->size = zp - z->data;
 }
